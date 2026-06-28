e45899d220dea66a765d0bc90accc570bc91b41b
arielbulacio-coder <arielbulacio-coder@users.noreply.github.com>
2026-06-28 16:16:26 -0300

    fix: actualizar base path para repo enprimerapersona/Malvinas

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
 
 export default defineConfig({
-  base: isGithubPages ? '/malvinas/' : '/',
+  base: isGithubPages ? '/Malvinas/' : '/',
   plugins: [react()]
 })
